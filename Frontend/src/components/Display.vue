8f99eb97e0ce899577b3631702f2761ed875f6f3
anhbaonguyenle <anhlenguyenbaoanh@gmail.com>
2022-11-30 02:38:10 +0700

    fix display product

--- Frontend/src/components/Display.vue
+++ Frontend/src/components/Display.vue
@@ -15,40 +15,17 @@
     
 </script>
 <template>
-    <div class="card" v-for="(product) in products" :key="product.id">
-        <img src="../assets/2.jpg" class="card-img-top" alt="..." />
+    <div class="row row-cols-1 row-cols-md-4 g-4" width= "250px" height= "250px" >
+    <div class="card" v-for="(product) in products" :key="product.id" >
+        <img :src="('http://localhost:3000/' + product.image)" />
                     <p text-align:center>{{ product.tennc }}</p>
-                    <h5 text-align:center>{{ product.gianc }} đ</h5>
+                    <h5 text-align:center>{{ product.gianc }}đ</h5>
                     <button><router-link :to="{ name: 'chitietsp' }">
                                         Mua ngay
                                     </router-link></button>
        <div>
-       
-        <p  
-            v-for="(product) in products"
-            :key="product.id"
-        >
-            <div class="col-md-4">
-                <div class="item">
-            {{ product.tennc }}
-            
-            {{ product.gianc }}
-            {{ product.maloai }}
-         
-
-            <img :src="('http://localhost:3000/' + product.image)" />
-                </div>
-        </div>
-   
-    <a href="#">
-      
-        
-    </a>
-           
-              
-        </p>
-      
     </div>
+</div>
         <!-- <p  
             v-for="(product) in products"
             :key="product.id"
